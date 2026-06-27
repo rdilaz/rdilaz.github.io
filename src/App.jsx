5b7c8d58e957a8533858b2158a894ed52382e060
Ryo N-D <r.nagakidilazzaro001@umb.edu>
2026-06-27 10:08:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import PrivacyPolicy from './pages/PrivacyPolicy';
+
 
 export default function App() {
   return (
