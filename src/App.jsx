92ca0d1d2be9e12d8b91351f52ab9deba2ba263b
Ryo N-D <123701725+rdilaz@users.noreply.github.com>
2026-06-27 10:15:51 -0400

    Remove PrivacyPolicy route from App
    Removed the PrivacyPolicy route from the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* If the URL is exactly "/", load the Home component */}
         <Route path="/" element={<Home />} />
-        
-        {/* If the URL matches this path, load the PrivacyPolicy component */}
-        <Route path="/spam-shredder/privacy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
   );
